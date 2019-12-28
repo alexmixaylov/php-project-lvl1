949f87a6bb2cf60a8b9cf3ed4acc0785c25c8b75
Alexmixaylov <alexmixaylov@gmail.com>
2019-12-28 23:18:41 +0300

    brain Even done

--- src/Even.php
+++ src/Even.php
@@ -5,13 +5,66 @@ namespace BrainGames\Even;
 use function cli\line;
 use function cli\prompt;
 
+const TIMES_TO_WIN = 3;
+
 function run()
+{
+    line("%s", TIMES_TO_WIN);
+    $count       = 0;
+    $userName    = greetAndGetName();
+    $askQuestion = function ($number, $userName) use ($count) {
+        line('Question: ' . $number);
+        $answer = prompt('Your answer ');
+
+        $rightAnswer = checkAnswer(isEven($number));
+
+        if ($answer == $rightAnswer) {
+            line('Correct!');
+
+            return true;
+
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $rightAnswer);
+            line("Let's try again, %s!", $userName);
+
+            return false;
+        }
+
+    };
+
+    do {
+        $pass = $askQuestion(getRandomNumber(), $userName);
+        $count++;
+        if ($count == TIMES_TO_WIN){
+            line('Congratulations, %s!', $userName);
+        }
+    } while ($count < TIMES_TO_WIN && $pass);
+
+
+}
+
+function checkAnswer($isEven)
+{
+    return $isEven ? 'yes' : 'no';
+}
+
+function isEven($number)
+{
+    return $number % 2 == 0;
+}
+
+function getRandomNumber()
+{
+    return rand(1, 100);
+}
+
+function greetAndGetName()
 {
     line('Welcome to Brain Games!');
     line('Answer "yes" if the number is even, otherwise answer "no".');
 
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    $name = prompt("\nMay I have your name?");
+    line("Hello, %s!\n", $name);
 
-
+    return $name;
 }
