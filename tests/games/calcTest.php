d9155e9ebbb3e4becda726b76ab234222f0b627a
Alexmixaylov <alexmixaylov@gmail.com>
2020-01-22 12:48:43 +0300

    use in the test was fixed

--- tests/games/calcTest.php
+++ tests/games/calcTest.php
@@ -3,14 +3,14 @@
 namespace Php\Package\Tests;
 
 use PHPUnit\Framework\TestCase;
-use BrainGames\games\calc;
+use function BrainGames\games\calc\calcExpression;
 
 class calcTest extends TestCase
 {
     public function testCalcExpression()
     {
-        $this->assertEquals(6, calc\calcExpression('*', 2, 3 ));
-        $this->assertEquals(5, calc\calcExpression('+', 2, 3 ));
-        $this->assertEquals(10, calc\calcExpression('-', 12, 2 ));
+        $this->assertEquals(6, calcExpression('*', 2, 3 ));
+        $this->assertEquals(5, calcExpression('+', 2, 3 ));
+        $this->assertEquals(10, calcExpression('-', 12, 2 ));
     }
 }
