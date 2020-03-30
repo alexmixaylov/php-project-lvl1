f5a9dd83baa9cf216448f51b638aba2cae9e32aa
Alexmixaylov <alexmixaylov@gmail.com>
2020-03-30 20:11:49 +0300

    Success display has been changed

--- src/engine.php
+++ src/engine.php
@@ -10,28 +10,28 @@ const TIMES_TO_WIN = 3;
 function play(string $gameDescription, callable $generateGameData)
 {
     // initial parameters
-    line("Welcome to Brain Games!" );
+    line("Welcome to Brain Games!");
+
     line($gameDescription);
+
     $userName = prompt("\nMay I have your name?");
     line("Hello, %s!\n", $userName);
 
     // game process
-    for ($i = 0; $i <= TIMES_TO_WIN; $i++) {
-        if ($i == TIMES_TO_WIN) {
-            line('Congratulations, %s!', $userName);
-            break;
-        }
-
+    for ($i = 0; $i < TIMES_TO_WIN; $i++) {
         [$question, $rightAnswer] = $generateGameData();
         line('Question: %s', $question);
         $userAnswer = prompt('Your answer ');
 
         if ($rightAnswer == $userAnswer) {
+            line('Correct!');
             continue;
         }
 
         line("'%s' is wrong answer ;(. Correct answer was '%s'.", $userAnswer, $rightAnswer);
         line("Let's try again, %s!", $userName);
-        break;
+        return;
     }
+
+    line('Congratulations, %s!', $userName);
 }
